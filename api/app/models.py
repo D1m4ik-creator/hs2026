fe4e7a7a66e653655b10032083afef1017319a2a
Dim4ik-creator <litvinov06062006@gmail.com>
2026-03-20 21:57:52 +0300

    Добавил Модель пользователя с ролями и Сериализатор регистраии и выхода

--- api/app/models.py
+++ api/app/models.py
@@ -1,4 +1,16 @@
 from django.db import models, AbstractUser
+from django.core.validators import RegexValidator
+
+
+login_validator = RegexValidator(
+    regex=r'^[a-zA-Z]+$',
+    message='Логин должен содержать только латинские буквы.'
+)
+
+fullname_validator = RegexValidator(
+    regex=r'^[а-яА-ЯёЁ\s\-]+$',
+    message='ФИО должно содержать только русские буквы.'
+)
 
 
 class Role(models.Model):
@@ -19,12 +31,16 @@ class User(AbstractUser):
     first_name = None
     last_name = None
 
-    login = models.CharField(max_length=150, unique=True)
-    full_name = models.CharField(max_length=255)
+    login = models.CharField(max_length=150, unique=True, validators=[login_validator])
+    full_name = models.CharField(max_length=255, validators=[fullname_validator])
     roles = models.ManyToManyField(Role, blank=True, related_name='users')
     is_deleted = models.BooleanField(default=False)
     data_registration = models.DateTimeField(auto_now_add=True)
 
+    def soft_delete(self):
+        self.is_deleted = True
+        self.save(update_fields=['is_deleted'])
+
     def __str__(self):
         return self.login
-    
+                                     
---
+++ api/app/serializers.py
@@ -0,0 +1,42 @@
+from rest_framework import serializers
+from django.contrib.auth.hashers import make_password
+from django.contrib.auth import get_user_model
+from django.contrib.auth.password_validation import validate_password
+from .models import User, Role
+import re
+
+User = get_user_model()
+
+class UserRegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+    password_confirm = serializers.CharField(write_only=True)
+
+    class Meta:
+        models = User
+        fields = ["login", "full_name", "password", "password_confirm"]
+
+    def validate(self, data):
+        password = data.get('password')
+        password_confirm = data.get('password_confirm')
+
+        if not re.match(r'^[a-zA-Z0-9!@#$%^&*()_+\-=\[\]{}|;:,.<>?]+$', password):
+            raise serializers.ValidationError({"password": "Пароль должен содержать только латинские буквы, цифры и специальные символы."})
+        if password != password_confirm:
+            raise serializers.ValidationError({"password_confirm": "Пароли не совпадают."})
+        return data
+    
+    def create(self, validated_data):
+        user_role = Role.objects.get(name='user')
+
+        user = User.objects.create_user(
+            login=validated_data.get('login'),
+            full_name=validated_data.get('full_name'),
+            password=validated_data['password'],
+        )
+        user.roles.add([user_role])
+
+        return user
+    
+    
+class LogoutSerializer(serializers.Serializer):
+    refresh = serializers.CharField(help_text="Refresh токен, который нужно отозвать")
